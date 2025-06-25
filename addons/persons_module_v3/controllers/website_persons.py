fb8843b33cbdb195b62d23d3e3475608bf342b7f
Mike Shmygel <m.shmygel@gmail.com>
2025-06-25 18:11:07 +0300

    Add frontend form for creating persons with company selection or manual input

--- addons/persons_module_v3/controllers/website_persons.py
+++ addons/persons_module_v3/controllers/website_persons.py
@@ -16,3 +16,28 @@ class WebsitePersons(http.Controller):
         )
         # Render the QWeb template with the list of persons
         return request.render("persons_module_v3.person_template", {"persons": persons})
+
+    @http.route("/persons/new", type="http", auth="public", website=True)
+    def person_form(self, **kwargs):
+        # Render a blank HTML form for user to submit person data
+        companies = request.env["res.company"].sudo().search([])
+        return request.render("persons_module_v3.person_form_template", {"companies": companies})
+
+    @http.route("/persons/create", type="http", auth="public", website=True, methods=["POST"], csrf=True)
+    def person_create(self, **post):
+        company_id = post.get("company_id")
+        new_company_name = post.get("new_company")
+
+        if not company_id and new_company_name:
+            # Create new company if only new name provided
+            company = request.env["res.company"].sudo().create({"name": new_company_name})
+            company_id = company.id
+
+        request.env["persons.person"].sudo().create({
+            "first_name": post.get("first_name"),
+            "last_name": post.get("last_name"),
+            "birthday": post.get("birthday"),
+            "sex": post.get("sex"),
+            "company_id": int(company_id) if company_id else False,
+        })
+        return request.redirect("/persons")
